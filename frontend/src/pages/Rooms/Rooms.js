83dbd80f9a6b50d62abf8ced704321f3e6c37f04
sumitmaithani123 <sumit@daffodilhealth.com>
2022-12-22 19:23:45 +0530

    resolved loading in fetching rooms

--- frontend/src/pages/Rooms/Rooms.js
+++ frontend/src/pages/Rooms/Rooms.js
@@ -1,17 +1,21 @@
 import React, { useEffect, useState } from "react";
 import AddRoomModal from "../../components/AddRoomModal.js/AddRoomModal";
 import RoomCard from "../../components/RoomCard/RoomCard";
+import Loader from "../../components/shared/Loader/Loader";
 import { getAllRooms } from "../../http";
 import styles from "./Rooms.module.css";
 
 const Rooms = () => {
   const [showModal, setShowModal] = useState(false);
   const [rooms, setRooms] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
+    setLoading(true);
     const fetchRooms = async () => {
       const { data } = await getAllRooms();
       setRooms(data);
+      setLoading(false);
     };
     fetchRooms();
   }, []);
@@ -25,30 +29,38 @@ const Rooms = () => {
 
   return (
     <>
-      <div className="container">
-        <div className={styles.roomHeaders}>
-          <div className={styles.left}>
-            <span className={styles.heading}>All voice rooms</span>
-            <div className={styles.searchBox}>
-              <img src="/images/search.png" alt="" />
-              <input className={styles.searchInput} />
+      {loading ? (
+        <div>
+          <Loader message="Loading, please wait.." />
+        </div>
+      ) : (
+        <>
+          <div className="container">
+            <div className={styles.roomHeaders}>
+              <div className={styles.left}>
+                <span className={styles.heading}>All voice rooms</span>
+                <div className={styles.searchBox}>
+                  <img src="/images/search.png" alt="" />
+                  <input className={styles.searchInput} />
+                </div>
+              </div>
+              <div className={styles.right}>
+                <button onClick={openModal} className={styles.startRoomButton}>
+                  <img src="/images/room_icon.png" alt="" />
+                  <span>Start a room</span>
+                </button>
+              </div>
+            </div>
+
+            <div className={styles.roomList}>
+              {rooms.map((room) => {
+                return <RoomCard key={room.id} room={room} />;
+              })}
             </div>
           </div>
-          <div className={styles.right}>
-            <button onClick={openModal} className={styles.startRoomButton}>
-              <img src="/images/room_icon.png" alt="" />
-              <span>Start a room</span>
-            </button>
-          </div>
-        </div>
-
-        <div className={styles.roomList}>
-          {rooms.map((room) => {
-            return <RoomCard key={room.id} room={room} />;
-          })}
-        </div>
-      </div>
-      {showModal && <AddRoomModal onClose={closeModal} />}
+          {showModal && <AddRoomModal onClose={closeModal} />}
+        </>
+      )}
     </>
   );
 };
